89d60495872e12720b843d547c33e77129974a65
caner <caner-ahmet.turan@dci-student.org>
2025-02-03 15:07:24 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import fragen from "readline-sync"
 
-const name = fragen.question("Wie heist du")
-let alter = fragen.question ("wie at bist du")
-const größe = fragen.question("wie groß bist du")
-const stadt = fragen.question ("in welcher stadt wohnen du")
-let hobby = fragen.question ("hast du hobbys wenn ja welche bei mehreren sag mir 2 davon")
+const name = fragen.question("Wie heist du ")
+let alter = fragen.question ("wie at bist du ")
+const größe = fragen.question("wie groß bist du ")
+const stadt = fragen.question ("in welcher stadt wohnen du ")
+let hobby = fragen.question ("hast du hobbys wenn ja welche bei mehreren sag mir 2 davon ")
 
 
-console.log(`Hallo ${name} du bist ${alter} jahre alt und bist ${größe}m groß und wohnst in der statd ${stadt} und das sind seine hobbys ${hobby}`)
+if (fragen){
+    console.log(`Hallo ${name} du bist ${alter} jahre alt und bist ${größe}m groß und wohnst in der statd ${stadt} und das sind seine hobbys ${hobby}.`)
+}
+
