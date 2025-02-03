77389ba7a4156b460f77c60ff305ec997f77d430
caner <caner-ahmet.turan@dci-student.org>
2025-02-03 15:41:26 +0100

    update

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const größe = fragen.question("wie groß bist du ")
 const stadt = fragen.question ("in welcher stadt wohnen du ")
 let hobby = fragen.question ("hast du hobbys wenn ja welche bei mehreren sag mir 2 davon ")
 
+console.log(`Hallo ${name} du bist ${alter} jahre alt und bist ${größe}m groß und wohnst in der statd ${stadt} und das sind seine hobbys ${hobby}.`)
 
-if (fragen){
-    console.log(`Hallo ${name} du bist ${alter} jahre alt und bist ${größe}m groß und wohnst in der statd ${stadt} und das sind seine hobbys ${hobby}.`)
+let bestätigung = fragen.question ("sind die angaben richtig? y:ja n:nein ")
+
+if (bestätigung.toLowerCase ==="y"){
+    console.log("danke für die deine angaben")
+} else{
+    console.log("okey du hast angeben  das die angeben falsch sind bitte beantworte die fragen erneut ")
 }
+
 
